3a0bbfa18019d34df688f4d0940b965170227e51
bolabolor <bolaskekse@gmail.com>
2023-05-19 17:33:31 +0200

    added some basic styling for friend

--- frontend/src/friend/FriendCard.tsx
+++ frontend/src/friend/FriendCard.tsx
@@ -4,7 +4,7 @@ type FriendCardProps = {
     friend : Friend
 }
 export default function FriendCard (props : FriendCardProps) {
-    return <div className="friend-card">
+    return <div className="friend-circle">
         <h3 className="friend-card_name">{props.friend.name}</h3>
         <h3 className="friend-card_image">{props.friend.url}</h3>
     </div>
